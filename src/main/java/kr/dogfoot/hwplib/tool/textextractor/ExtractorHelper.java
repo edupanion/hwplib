c82fb17e58937e8f63ac6e2461eb8f421cb49301
hanbitkim <hanbit2.kim@gmail.com>
2023-03-07 14:25:13 +0900

    remove span default style

--- src/main/java/kr/dogfoot/hwplib/tool/textextractor/ExtractorHelper.java
+++ src/main/java/kr/dogfoot/hwplib/tool/textextractor/ExtractorHelper.java
@@ -6,7 +6,7 @@ public class ExtractorHelper {
         insertTag(option, sb, "<span style=\"margin-left:" + marginLeft + "px;\"/>");
     }
     public static void appendNormalStartTag(TextExtractOption option, StringBuffer sb) {
-        insertTag(option, sb, "<span style=\"vertical-align: baseline;\">");
+        insertTag(option, sb, "<span>");
     }
 
     public static void appendNormalEndTag(TextExtractOption option, StringBuffer sb) {

4060c4949187216e456f8b6028d8d98024cd6e47
hanbitkim <hanbit2.kim@gmail.com>
2023-03-20 11:36:25 +0900

    add similar image tag

--- src/main/java/kr/dogfoot/hwplib/tool/textextractor/ExtractorHelper.java
+++ src/main/java/kr/dogfoot/hwplib/tool/textextractor/ExtractorHelper.java
@@ -16,20 +16,15 @@ public class ExtractorHelper {
     }
 
     public static void appendEquationTag(TextExtractOption option, StringBuffer sb, String data) {
-        insertTag(option, sb, "<span id=\"equation\">");
-        String convertedText = data;
-        Matcher ltMatcher = ltPattern.matcher(data);
-        while (ltMatcher.find()) {
-            final String matcherText = ltMatcher.group();
-            convertedText = convertedText.replace(matcherText, "&lt; " + ltMatcher.group(2));
+        String[] equations = data.split("\uDB80\uDC40");
+        for (int i = 0; i < equations.length; i++) {
+            if (i > 0) {
+                sb.append("<img src=\"https://img-wonriedu.cceapi.com/q/d1/52/q.53586/104.jpg\" style=\"width:18px; height:9px;\">");
+            }
+            insertTag(option, sb, "<span id=\"equation\">");
+            addEquation(sb, equations[i]);
+            insertTag(option, sb, "</span>");
         }
-        Matcher gtMatcher = gtPattern.matcher(data);
-        while (gtMatcher.find()) {
-            final String matcherText = gtMatcher.group();
-            convertedText = convertedText.replace(matcherText, gtMatcher.group(1) + "&gt;");
-        }
-        sb.append(convertedText);
-        insertTag(option, sb, "</span>");
     }
 
     public static void appendTableTag(TextExtractOption option, StringBuffer sb, String data) {
@@ -48,5 +43,20 @@ public class ExtractorHelper {
         if (option.isInsertTag()) {
             sb.append(tag);
         }
+    }
+
+    private static void addEquation(StringBuffer sb, String data) {
+        String convertedText = data;
+        Matcher ltMatcher = ltPattern.matcher(data);
+        while (ltMatcher.find()) {
+            final String matcherText = ltMatcher.group();
+            convertedText = convertedText.replace(matcherText, "&lt; " + ltMatcher.group(2));
+        }
+        Matcher gtMatcher = gtPattern.matcher(data);
+        while (gtMatcher.find()) {
+            final String matcherText = gtMatcher.group();
+            convertedText = convertedText.replace(matcherText, gtMatcher.group(1) + "&gt;");
+        }
+        sb.append(convertedText);
     }
 }
